3dff9eba3e89910078c32f3323ef9b58bad35480
Jeff L <jeff@langrsoft.com>
2021-11-09 18:49:36 -0700

    init exercise for name normalizer

--- src/util/NameNormalizer.ts
+++ src/util/NameNormalizer.ts
@@ -1,5 +1,36 @@
-class NameNormalizer {
+class ParsedName {
+  private readonly parts: string[];
+  private readonly name: String;
 
+  constructor(name: String) {
+    this.name = name;
+    this.parts = name.split(' ')
+  }
+
+  baseName() {
+    return this.name;
+  }
+
+  isMononym() {
+    return this.parts.length === 1
+  }
+
+  last() {
+    return this.parts[1]
+  }
+
+  first() {
+    return this.parts[0]
+  }
+}
+
+class NameNormalizer {
+  normalize(name: String): String {
+    const parsedName = new ParsedName(name)
+    if (parsedName.isMononym())
+      return parsedName.baseName()
+    return `${parsedName.last()}, ${parsedName.first()}`
+  }
 }
 
 export default NameNormalizer
--- test/util/NameNormalizer.spec.ts
+++ test/util/NameNormalizer.spec.ts
@@ -1,9 +1,45 @@
-import { expect } from 'chai'
+import {expect} from 'chai'
 import NameNormalizer from '../../src/util/NameNormalizer'
 
 describe('#NameNormalizer', () => {
-    it('does stuff', () => {
-        expect(true).to.equal(true)
-    })
+  let normalizer
+  beforeEach(() => {
+    normalizer = new NameNormalizer()
+  })
 
+  it('returns empty when passed empty string', () => {
+    expect(normalizer.normalize('')).to.equal('')
+  })
+
+  it('returns single word name', () => {
+    expect(normalizer.normalize('Plato')).to.equal('Plato')
+  })
+
+  it('swaps first and last names', () => {
+    expect(normalizer.normalize('Haruki Murakami')).to.equal('Murakami, Haruki')
+  })
+
+  xit('trims leading and trailing whitespace', () => {
+    expect(normalizer.normalize('  Big Boi   ')).to.equal('Boi, Big')
+  })
+
+  xit('initializes middle name', () => {
+    expect(normalizer.normalize('Henry David Thoreau')).to.equal('Thoreau, Henry D.')
+  })
+
+  xit('does not initialize one letter middle name', () => {
+    expect(normalizer.normalize('Harry S Truman')).to.equal('Truman, Harry S')
+  })
+
+  xit('initializes each of multiple middle names', () => {
+    expect(normalizer.normalize('Julia Scarlett Elizabeth Louis-Dreyfus')).to.equal('Louis-Dreyfus, Julia S. E.')
+  })
+
+  xit('appends suffixes to end', () => {
+    expect(normalizer.normalize('Martin Luther King, Jr.')).to.equal('King, Martin L., Jr.')
+  })
+
+  xit('throws when name contains two commas', () => {
+    expect(() => normalizer.normalize('Thurston, Howell, III')).to.throw()
+  })
 })
