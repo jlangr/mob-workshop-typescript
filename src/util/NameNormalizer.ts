cafa136f551637cb0ad6d81587ef200f65e7d848
Jeff L <jeff@langrsoft.com>
2021-11-09 18:16:54 -0700

    init

---
+++ src/util/NameNormalizer.ts
@@ -0,0 +1,5 @@
+class NameNormalizer {
+
+}
+
+export default NameNormalizer
---
+++ test/util/NameNormalizer.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+import NameNormalizer from '../../src/util/NameNormalizer'
+
+describe('#NameNormalizer', () => {
+    it('does stuff', () => {
+        expect(true).to.equal(true)
+    })
+
+})
